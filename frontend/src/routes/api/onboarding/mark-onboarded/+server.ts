647d69af717885d8668467335ea1db1b722bb097
EDRipper <euan@hackclub.com>
2026-05-14 15:28:45 -0400

    fix the proxy on the onboarding route

---
+++ frontend/src/routes/api/onboarding/mark-onboarded/+server.ts
@@ -0,0 +1,11 @@
+import { env } from '$env/dynamic/private';
+import { proxyWithRefresh } from '$lib/server/auth';
+import type { RequestHandler } from './$types';
+
+const BACKEND_URL = env.BACKEND_URL ?? 'http://localhost:3001';
+
+export const POST: RequestHandler = async ({ cookies }) => {
+	return proxyWithRefresh(cookies, `${BACKEND_URL}/api/onboarding/mark-onboarded`, {
+		method: 'POST'
+	});
+};
